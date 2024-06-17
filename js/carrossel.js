63599473b381c5c8e2fca8d6befcea59e6c3340b
Vinicius Nascimento <vinicius311006@gmail.com>
2024-06-17 18:58:43 -0300

    first commit

---
+++ js/carrossel.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".carrossel").slick({
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      infinite: true,
+      autoplaySpeed: 3000,
+    });
+  });
+  
