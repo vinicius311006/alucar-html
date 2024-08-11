aeceea72ebd5a416a24734b55d7759aac3481976
vinicius <vinicius311006@gmail.com>
2024-08-10 22:22:59 -0300

    pegando data

--- js/localizacao.js
+++ js/localizacao.js
@@ -1,22 +1,37 @@
 $(document).ready(function () {
 
-  function formatDateToYYYYMMDD(dateString) {
-    // Cria um objeto Date a partir da string
-    const date = new Date(dateString);
-
-    // Obtém os componentes da data
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-
-    // Retorna a data no formato YYYY-MM-DD
-    return `${year}-${month}-${day}`;
-  }
-
   $('#btnContinuar').on('click', function () {
+
+    $('#form-r').on('click', function (event) {
+      event.preventDefault()
+    })
+
     var dataInicial = $('#data-inicial').val()
     var dataFinal = $('#data-final').val()
-    
+
+    var timeInical = $('#time-inical').val()
+    var timeFinal = $('#time-final').val()
+
+    if (dataInicial != '' && dataFinal != '' && timeInical != '' && timeFinal != '') {
+      // Converte as strings de data para objetos Date
+      var date1 = new Date(dataInicial);
+      var date2 = new Date(dataFinal);
+
+      // Normaliza as datas para o início do dia (00:00:00)
+      date1.setHours(0, 0, 0, 0);
+      date2.setHours(0, 0, 0, 0);
+
+      // Calcula a diferença em milissegundos
+      var ContadorHoras = Math.abs(date2 - date1);
+
+      // Converte a diferença para dias
+      var contadorDias = Math.floor(ContadorHoras / (1000 * 60 * 60 * 24));
+      
+    }
+    else {
+      location.reload()
+      alert('Preenche todos os campos')
+    }
   })
 
 })
